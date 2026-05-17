551cb2ba32951d44704b6f9810d81eb54905a97a
Iancu Victor-Iuliu <iancu27victor@gmail.com>
2026-05-17 13:16:23 +0300

    first push in this repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+//import { useState } from 'react'
+import { InputCard } from './components';
+import './App.css'
+
+function App() {
+  return <>
+  <InputCard name='Hello'/>
+  </>
+}
+
+export default App
---
+++ src/components/inputCard.jsx
@@ -0,0 +1,7 @@
+function InputCard({name = 'default name'}){
+    return <>
+    <h1>{name}</h1>
+    </>
+}
+
+export default InputCard;
